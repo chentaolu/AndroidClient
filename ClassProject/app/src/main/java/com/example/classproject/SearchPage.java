e2174ff1effd2839e5f9bdf84023be4823f6256b
CHY <int88ra2260@gmail.com>
2022-05-21 16:28:03 +0800

    I Am so handsome

--- ClassProject/app/src/main/java/com/example/classproject/SearchPage.java
+++ ClassProject/app/src/main/java/com/example/classproject/SearchPage.java
@@ -3,8 +3,11 @@ package com.example.classproject;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 public class SearchPage extends AppCompatActivity {
     EditText schoolname;
@@ -15,6 +18,16 @@ public class SearchPage extends AppCompatActivity {
 
         schoolname = (EditText)findViewById(R.id.edt1);
 
+        Spinner spinner = (Spinner) findViewById(R.id.spinner1);
+        ArrayAdapter<CharSequence> adapter =
+                ArrayAdapter.createFromResource(this,
+                        R.array.planets_array,
+                        android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setSelection(2, false);
+        spinner.setOnItemSelectedListener(spnOnItemSelected);
+
         Button submit = (Button)findViewById(R.id.button1);
         submit.setOnClickListener(new Button.OnClickListener(){
             @Override
@@ -23,4 +36,16 @@ public class SearchPage extends AppCompatActivity {
             }
         });
     }
+    private AdapterView.OnItemSelectedListener spnOnItemSelected
+            = new AdapterView.OnItemSelectedListener() {
+        public void onItemSelected(AdapterView<?> parent, View view,
+                                   int pos, long id) {
+            String sPos=String.valueOf(pos);
+            String sInfo=parent.getItemAtPosition(pos).toString();
+            //String sInfo=parent.getSelectedItem().toString();
+        }
+        public void onNothingSelected(AdapterView<?> parent) {
+
+        }
+    };
 }
--- ClassProject/app/src/main/java/com/example/classproject/mapAct.java
+++ ClassProject/app/src/main/java/com/example/classproject/mapAct.java
@@ -35,7 +35,6 @@ public class mapAct extends FragmentActivity implements OnMapReadyCallback {
                 startActivity(schoolsearch);
             }
         });
-
     }
 
     @Override
