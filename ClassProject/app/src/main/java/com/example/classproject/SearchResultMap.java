b76748ebc4e037bcd0d412d9292ee8a134896c87
chingtingtu <d0753158@o365.fcu.edu.tw>
2022-05-25 11:15:12 +0800

    add animation and navigation

--- ClassProject/app/src/main/java/com/example/classproject/SearchResultMap.java
+++ ClassProject/app/src/main/java/com/example/classproject/SearchResultMap.java
@@ -3,7 +3,12 @@ package com.example.classproject;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -11,6 +16,7 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -24,6 +30,9 @@ public class SearchResultMap extends AppCompatActivity implements OnMapReadyCall
     static public String selectSchool = "";
     static public String selectCountry = "";
     public LatLng selectLatLng;
+    Button start, stop, launch1, launch2, launch3;
+    private GoogleMap mMap;
+    String uriString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,18 +60,82 @@ public class SearchResultMap extends AppCompatActivity implements OnMapReadyCall
             e.printStackTrace();
         }
 
-        
+        start = (Button)findViewById(R.id.btstart);
+        stop = (Button)findViewById(R.id.btstop);
+
+        launch1 = (Button)findViewById(R.id.btdrive);
+        launch1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                uriString = String.format("google.navigation:q=%f,%f,&mode=d", selectLatLng.latitude, selectLatLng.longitude);
+                launchMap();
+            }
+        });
+        launch2 = (Button)findViewById(R.id.btwalk);
+        launch2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                uriString = String.format("google.navigation:q=%f,%f,&mode=w", selectLatLng.latitude, selectLatLng.longitude);
+                launchMap();
+            }
+        });
+        launch3 = (Button)findViewById(R.id.btbicycle);
+        launch3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                uriString = String.format("google.navigation:q=%f,%f,&mode=b", selectLatLng.latitude, selectLatLng.longitude);
+                launchMap();
+            }
+        });
+
     }
+
+    public void launchMap(){
+        Uri intentUri = Uri.parse(uriString);
+        Intent intent = new Intent(Intent.ACTION_VIEW, intentUri);
+        intent.setPackage("com.google.android.apps.maps");
+
+        if(intent.resolveActivity(getPackageManager())!=null){
+            startActivity(intent);
+        }
+    }
+
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
 
         BitmapDescriptor descriptor = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE);
         MarkerOptions marker = new MarkerOptions().position(selectLatLng).title(selectSchool).icon(descriptor);
 
         googleMap.addMarker(marker);
 
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(selectLatLng));
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(MainActivity.currentLocation));
         googleMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+
+        final CameraPosition cameraPosition = new CameraPosition.Builder().target(selectLatLng).zoom(12).build();
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition),2000, new GoogleMap.CancelableCallback(){
+                    @Override
+                    public void onFinish() {
+                        Log.d("Map", "Animation finished");
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        Log.d("Map", "Animation interrupted");
+                    }
+                });
+            }
+        });
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mMap.stopAnimation();
+            }
+        });
     }
 }
